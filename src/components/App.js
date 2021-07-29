c8c6c2e022df962a087448574b72d3527e55cccb
steven chen <schen0096@gmail.com>
2021-07-29 03:52:23 -0400

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React, { useEffect, useState } from 'react';
+
+function App(){
+
+    const [pics, setPics] = useState('')
+
+    useEffect(() => {
+        console.log("Fetching data...");
+        fetch("https://dog.ceo/api/breeds/image/random/1")
+          .then(res => res.json())
+          .then(data => {console.log("Data fetched!", data);
+          setPics(data.message); 
+        });
+    }, []
+    )
+
+    if (pics){
+      return(
+          <img src = {pics} alt = "A Random Dog"/>  
+      )
+    }
+
+    console.log(pics)
+
+    return (
+        <p>Loading...</p>
+    )
+}
+
+export default App;
